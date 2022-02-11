39592cb2a8feb8caee74278f8d826f317780fe47
mako <pietrzak.maciek.pv@gmail.com>
2022-02-11 18:00:41 +0100

    succeed test button onClick behavior

--- Heroslandidle/app/src/main/java/com/mako/heroslandidle/BuildingsAdapter.java
+++ Heroslandidle/app/src/main/java/com/mako/heroslandidle/BuildingsAdapter.java
@@ -18,6 +18,7 @@ public class BuildingsAdapter extends RecyclerView.Adapter<BuildingsAdapter.Buil
     private Player player;
     private String[] buildingsTypes,
                     buildingsDescriptions;
+    private int[] buildingsMaxLvls;
 
     public BuildingsAdapter(Context context, Resources res, Player player) {
         this.context = context;
@@ -35,6 +36,7 @@ public class BuildingsAdapter extends RecyclerView.Adapter<BuildingsAdapter.Buil
     private void initialize(){
         buildingsTypes = res.getStringArray(R.array.buildings_types);
         buildingsDescriptions = res.getStringArray(R.array.buildings_descriptions);
+        buildingsMaxLvls = res.getIntArray(R.array.buildings_max_lvls);
     }
 
     @NonNull
@@ -50,6 +52,20 @@ public class BuildingsAdapter extends RecyclerView.Adapter<BuildingsAdapter.Buil
     public void onBindViewHolder(@NonNull BuildingsViewHolder holder, int position) {
         holder.type.setText(buildingsTypes[position]);
         holder.description.setText(buildingsDescriptions[position]);
+
+        int currentBuildingLvl = Integer.parseInt(holder.lvl.getText().toString());
+        if(isMaxBuildingLvl(position, holder.button, currentBuildingLvl)){
+            setMaxAndDisable(holder.button);
+        } else {
+            holder.button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    build();
+                    TextView tv_lvl = view.findViewById(R.id.buildings_row_lvl_txt_numeric);
+
+                }
+            });
+        }
     }
 
     @Override
@@ -57,16 +73,34 @@ public class BuildingsAdapter extends RecyclerView.Adapter<BuildingsAdapter.Buil
         return buildingsTypes.length;
     }
 
+    private boolean isMaxBuildingLvl(int position ,Button button, int lvl){
+        int maxBuildingLvl = buildingsMaxLvls[position];
+
+
+        return (lvl == maxBuildingLvl && button.isEnabled());
+    }
+
+    private void setMaxAndDisable(Button button){
+        button.setText(R.string.btn_build_max);
+        button.setEnabled(false);
+    }
+
+    private void build(){
+        System.out.println("build");
+    }
+
     public class BuildingsViewHolder extends RecyclerView.ViewHolder {
 
         private TextView type,
-                        description;
+                        description,
+                        lvl;
         private Button button;
 
         public BuildingsViewHolder(@NonNull View itemView) {
             super(itemView);
             this.type = itemView.findViewById(R.id.buildings_row_type_txt);
             this.description = itemView.findViewById(R.id.buildings_row_description_txt);
+            this.lvl = itemView.findViewById(R.id.buildings_row_lvl_txt_numeric);
             this.button = itemView.findViewById(R.id.buildings_row_build_btn);
         }
     }
