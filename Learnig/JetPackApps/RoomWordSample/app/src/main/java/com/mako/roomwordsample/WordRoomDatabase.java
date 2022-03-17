d324f3078733b61ef8854b2492f30a41d81837f4
mako <pietrzak.maciek.pv@gmail.com>
2022-03-17 09:31:05 +0100

    Populate the database

--- Learnig/JetPackApps/RoomWordSample/app/src/main/java/com/mako/roomwordsample/WordRoomDatabase.java
+++ Learnig/JetPackApps/RoomWordSample/app/src/main/java/com/mako/roomwordsample/WordRoomDatabase.java
@@ -2,9 +2,11 @@ package com.mako.roomwordsample;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,11 +20,28 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     private static final int NUMBER_OF_THREADS = 4;
     static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            databaseWriteExecutor.execute(() ->{
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+
+                Word word = new Word("Hello");
+                dao.insert(word);
+                word = new Word("World");
+                dao.insert(word);
+            });
+        }
+    };
     static WordRoomDatabase getDatabase(final Context context){
         if (INSTANCE == null){
             synchronized (WordRoomDatabase.class){
                 if (INSTANCE == null){
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
